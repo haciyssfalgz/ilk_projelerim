08073cd2a50f86a77519f0fab2f090113ededd01
haciyssfalgz <155294486+haciyssfalgz@users.noreply.github.com>
2025-08-21 23:24:19 +0300

    Add files via upload

---
+++ sozalcikod.py
@@ -0,0 +1,6 @@
+mesaj=input("mesaj gir ")
+
+mesaj = mesaj.lower()
+mesaj = mesaj.title()
+print(mesaj)
+# bu kod yazılan rasgele bi yazının tüm başm harflerini büyük yapar ve diğer harfleri küçük yapar.
