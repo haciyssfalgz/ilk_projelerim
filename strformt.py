237b81ea632c6d7f6ed9e1b0356e8c430bd05290
hacı algz <alagozhaciyusuf@gmail.com>
2025-08-21 23:06:17 +0300

    komut ekleme

---
+++ strformt.py
@@ -0,0 +1,23 @@
+urun_adi = "elma "
+fiyat= 15 
+miktar = 5.0
+
+sonuç1 = str(miktar) + " kilo "+ urun_adi +" için " + str(miktar*fiyat) + " tl öde"
+
+print(sonuç1)
+
+sonuç2 = "{2} kilo {0} için {1} tl öde".format(urun_adi,(fiyat*miktar),miktar)
+
+print(sonuç2)
+
+sonuç3 = "{m} kilo {u} için {tf} tl öde".format( m = miktar, u = urun_adi, tf = (fiyat*miktar))
+
+print(sonuç3)
+
+sonuç4 = "{} kilo {} için {} tl öde".format(miktar,urun_adi,(fiyat*miktar))
+
+print(sonuç4)
+
+sonuç5 = f"{miktar} kilo {urun_adi} için {miktar*fiyat} tl öde"
+
+print(sonuç5)
