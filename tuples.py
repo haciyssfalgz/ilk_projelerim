2424b86f5ae115c77b0bd9bcee16319f727c97f6
hacı algz <alagozhaciyusuf@gmail.com>
2025-08-24 15:30:30 +0300

    ilk hali

---
+++ tuples.py
@@ -0,0 +1,8 @@
+mytuple = ("apple", "banana", "cherry")
+listem = list(mytuple)
+print(type(mytuple))
+# sıralanabilir tekraralanabilir elemanlar guncellenemez
+# parantez ile tanımlanır
+print(type(listem))
+# sıralanabilir tekraralanabilir elemanlar guncellenebilir
+# köşeli parantez ile tanımlanır
